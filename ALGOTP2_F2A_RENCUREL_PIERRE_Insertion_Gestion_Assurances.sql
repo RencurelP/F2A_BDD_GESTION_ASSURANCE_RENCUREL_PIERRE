e6cb83ffd5f888a36b55a749d8de44cb1e6fdc13
RencurelP <p.rencurel@ludus-academie.com>
2017-06-01 15:58:40 +0200

    F2A_BDD_GESTION_ASSURANCE_RENCUREL_PIERRE

--- ALGOTP2_F2A_RENCUREL_PIERRE_Insertion_Gestion_Assurances.sql
+++ ALGOTP2_F2A_RENCUREL_PIERRE_Insertion_Gestion_Assurances.sql
@@ -1,21 +1,21 @@
 -- Insertion des donnees --
 
 INSERT INTO CLIENT (NUMCLIENT, NOM, ADRESSE)
-VALUES (1, 'RENCUREL', 'Rue de la Paix'),
+VALUES (1, 'RENCUREL', 'Rue de la Paix'),	-- Clients --
 		(2, 'ROGER', 'Avenue de Gal de Gaulle'),
 		(3, 'BERNARD', 'Cite de la peur'),
 		(4, 'Dark Vador', 'Etoile Noire'),
 		(5, 'Sauron', 'Barad Dur');
 
 INSERT INTO CONTRAT (SIGNATAIRE, NUMCTR, TYPE, DATESIGN)
-VALUES ('ROGER', 'C400', 'C', '%Y - %m - %d'),
-		('Dark Vador', '1977', 'D', '%Y - %m - %d'),
+VALUES ('ROGER', 'C400', 'C', '%Y - %m - %d'), -- Client avec un contrat --
+		('Dark Vador', '1977', 'D', '%Y - %m - %d'), -- CLient avec plusieurs véhicules
 		('Dark Vador', '1983', 'D', '%Y - %m - %d');
 
 INSERT INTO VEHICULE (NUMVEH, MARQUE, MODELE, ANNEE, CYLINDREE, SIGNATAIRE, NUMCTR, NUMCLIENT)
-VALUES (1, 'TIE', 'Fighter', '%Y - %m - %d', '1500 cm3', 'Dark Vador', '1977', 1),
+VALUES (1, 'TIE', 'Fighter', '%Y - %m - %d', '1500 cm3', 'Dark Vador', '1977', 1), 
 		(2, 'Star', 'Death', '%Y - %m - %d', '1500 Hm3', 'Dark Vador', '1983', 1),
-		(3, 'Troll', 'Olog Hai', '%Y - %m - %d', '3m3', 'Sauron', '', 5);
+		(3, 'Troll', 'Olog Hai', '%Y - %m - %d', '3m3', 'Sauron', '', 5); -- CLient avec vehicule non assurer
 
 INSERT INTO ACCIDENT (NUMACC, DATEACC, MONTANT)
 VALUES (1, '%Y - %m - %d', 1965.2),
@@ -25,8 +25,8 @@ VALUES (1, '%Y - %m - %d', 1965.2),
 		(5, '%Y - %m - %d', 1236);
 
 INSERT INTO IMPLICATION (NUMACC, NUMVEH)
-VALUES (1, 2),
+VALUES (1, 2),	-- Vehicule impliquer dans un accident
 		(2, 3),
 		(3, 2),
-		(4, 3),
+		(4, 3),	-- deux véhicules dans le même accident
 		(4, 1);
